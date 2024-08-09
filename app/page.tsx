baa2d22d7772b7d97177a51dd747d29526b9edcb
Jose Collas <joseherminiocollas@goatstone.com>
2024-08-09 10:59:50 -0400

    Update UI to show basic functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-export const metadata = {
-  title: "App Router",
-};
+import AaartoButton from "./components/AaartoButton";
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <>
+      <h1>Aaarto</h1>
+      <h3>Create and Mint Art</h3>
+      <svg
+        version="1.1"
+        width="300"
+        height="300"
+        xmlns="http://www.w3.org/2000/svg"
+        id="art"
+      >
+        <rect width="100%" height="100%" fill="gray" />
+        <circle cx="150" cy="100" r="80" fill="green" />
+        <text x="150" y="125" font-size="50" text-anchor="middle" fill="white">
+          Aaarto
+        </text>
+      </svg>
+      <AaartoButton />
+    </>
+  );
 }
