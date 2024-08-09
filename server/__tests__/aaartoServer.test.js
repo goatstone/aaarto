cda0c53a6eb1ceaf36ac4ce068060d13cf2f0753
Jose Herminio Collas <goatstone@gmail.com>
2024-08-09 16:20:26 -0400

    Test if the expected method has been called

--- server/__tests__/aaartoServer.test.js
+++ server/__tests__/aaartoServer.test.js
@@ -1,6 +1,16 @@
+import aaartoServer from "../aaartoServer";
+
 describe("aaartoServer", () => {
-    it("should be truthy", () => {
-      expect(0).toBe(0);
-    });
+  const mock ={
+    set:jest.fn(),
+    use:jest.fn(),
+    get:jest.fn(),
+    post:jest.fn(),
+    listen:jest.fn()
+  }
+
+  it("should call expected methods", () => {
+    aaartoServer(mock, 8000)
+    expect(mock.post.mock.calls.length).toBe(2);
   });
-  
+});
