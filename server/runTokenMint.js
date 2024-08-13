3a019ac6b9480fc489c8bac48a7a354d3546e6d0
Jose Herminio Collas <goatstone@gmail.com>
2024-08-13 17:17:03 +0000

    Use supplykey generated on creation, fix issues

--- server/runTokenMint.js
+++ server/runTokenMint.js
@@ -5,15 +5,14 @@ import getID from "#root/server/getID.js";
 
 const ID = getID();
 const client = getClient(ID.operatorId, ID.operatorKey);
-// const supplyKey = PrivateKey.generate();
 const CID = [
   Buffer.from(
     "ipfs://bafyreiao6ajgsfji6qsgbqwdtjdu5gmul7tv2v3pd6kjgcw5o65b2ogst4/metadata.json"
   ),
 ];
-const tokenId = "0.0.4677756";
-const sk = '0xc00598834bebf0818badc584951abc69174ca44f411cc22bade93a48cfa8131d'
-const supplyKey= PrivateKey.fromStringED25519(sk)
-tokenMint(client, tokenId, CID, supplyKey).then((response) =>
-  console.log(`tokenMint: ${response} ${new Date()}`)
-);
+const tokenId = "0.0.4678180";
+const supplyKey = PrivateKey.fromStringED25519("302e020100300506032b657004220420a3e3c2e2c0e7f1ddd4068028df3a8d5a52ef7f66ae44aa78e82c2b116ad2272f");
+console.log("ss", supplyKey);
+tokenMint(client, tokenId, CID, supplyKey).then((response) => {
+  console.log(`tokenMint: ${new Date()}`, response.serials, response.tokenId);
+});
