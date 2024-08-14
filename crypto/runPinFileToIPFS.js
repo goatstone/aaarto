4da0b952f5e749faba3e1beff571af5c865689fc
Jose Collas <joseherminiocollas@goatstone.com>
2024-08-14 14:42:15 -0400

    Refactor

--- crypto/runPinFileToIPFS.js
+++ crypto/runPinFileToIPFS.js
@@ -1,18 +1,15 @@
 import pinFileToIPFS from "./pinFileToIPFS.js";
-import dotenv from "dotenv";
+import getPinataJWT from "./util/getPinataJWT.js";
 
-dotenv.config();
-
-const JWT = process.env.PINATA_KEY;
 // upload SVG art to IPFS
 const art = `<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg" id="art">
   <rect width="100%" height="100%" fill="gray" />
   <circle cx="150" cy="100" r="80" fill="green" />
   <text x="150" y="125" font-size="50" text-anchor="middle" fill="white">
-    ART 10
+    ART 11
   </text>
 </svg>`;
-pinFileToIPFS(art, JWT, "aaarto.svg", "image/svg+xml").then((response) => {
+pinFileToIPFS(art, getPinataJWT(), "aaarto.svg", "image/svg+xml").then((response) => {
   console.log("pinFileToIPFS: ", response);
 });
 // Upload a JSON metadata to Pinata IPFS
