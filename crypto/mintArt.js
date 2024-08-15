befb0fe2a5dc6268f35db5d98229b1221d05d39c
Jose Collas <joseherminiocollas@goatstone.com>
2024-08-15 12:05:48 -0400

    Add getConfig, use in minArt

--- crypto/mintArt.js
+++ crypto/mintArt.js
@@ -5,15 +5,12 @@ import tokenMint from "#root/crypto/tokenMint.js";
 import getClient from "#root/crypto/util/getClient.js";
 import getID from "#root/crypto/util/getID.js";
 import getMetaData from "#root/crypto/util/getMetaData.js";
+import getConfig from "#root/crypto/util/getConfig.js";
 
 const ID = getID();
 const client = getClient(ID.operatorId, ID.operatorKey);
-// An NFT token called Goatstone
-const tokenId = "0.0.4678180";
-// supply key from above token
-const supplyKey = PrivateKey.fromStringED25519(
-  "302e020100300506032b657004220420a3e3c2e2c0e7f1ddd4068028df3a8d5a52ef7f66ae44aa78e82c2b116ad2272f"
-);
+const { tokenId, supplyKey } = getConfig();
+const sK = PrivateKey.fromStringED25519(supplyKey);
 /**
  * Take a string of art and mint it
  */
@@ -33,8 +30,12 @@ const mintArt = async (art) => {
     "applicaton/json"
   );
   // Mint the NFT
-  const CID = [Buffer.from(`https://ipfs.io/ipfs/${pinDataResponse.IpfsHash}/metadata.json`)];
-  const mintResponse = await tokenMint(client, tokenId, CID, supplyKey);
+  const CID = [
+    Buffer.from(
+      `https://ipfs.io/ipfs/${pinDataResponse.IpfsHash}/metadata.json`
+    ),
+  ];
+  const mintResponse = await tokenMint(client, tokenId, CID, sK);
 
   return { pinImageResponse, pinDataResponse, mintResponse };
 };
---
+++ crypto/util/getConfig.js
@@ -0,0 +1,11 @@
+/**
+ * Get configuration settings
+ * @returns configuration object
+ */
+const getConfig = () => ({
+  tokenId: "0.0.4678180",
+  supplyKey:
+    "302e020100300506032b657004220420a3e3c2e2c0e7f1ddd4068028df3a8d5a52ef7f66ae44aa78e82c2b116ad2272f",
+});
+
+export default getConfig;
