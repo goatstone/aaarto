9efbc3604696d40aa410a232a2b6e2d791846e66
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2024-12-30 21:23:40 +0000

    add hashconnect

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Use 'react-dom/client' instead of 'react-dom'
+import hashconnect from 'hashconnect';
 
 const App: React.FC = () => {
   return <h1>Hello, React and TypeScript!</h1>;
@@ -10,3 +11,5 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);  // Create a root
   root.render(<App />);  // Render the App
 }
+
+console.log(hashconnect);
