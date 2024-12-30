d914f9ab4b497d882c3159f2fbc999af92ef9134
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2024-12-30 21:46:00 +0000

    refactor files, seperate App

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnect';
+console.log('HashConnect', HashConnect);
+
+const App: React.FC = () => {
+    return <h1>Hello, React and TypeScript!</h1>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Use 'react-dom/client' instead of 'react-dom'
-import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnect';
-
-const App: React.FC = () => {
-  return <h1>Hello, React and TypeScript!</h1>;
-};
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);  // Create a root
-  root.render(<App />);  // Render the App
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
 }
-
-console.log('HashConnect', HashConnect);
