a099b89a92b93660a4299b50c1ff5485cef0a119
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2024-12-30 21:55:37 +0000

    add layout to build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,55 @@ import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnec
 console.log('HashConnect', HashConnect);
 
 const App: React.FC = () => {
-    return <h1>Hello, React and TypeScript!</h1>;
+    return (
+        <div>
+            <h1>
+                <a href="https://github.com/JoseHerminioCollas/aaarto/" target="new">
+                    Aaarto
+                </a>
+                <a href="http://goatstone.com" target="new">goatstone</a>
+            </h1>
+            <svg
+                version="1.1"
+                width="500"
+                height="500"
+                xmlns="http://www.w3.org/2000/svg"
+                id="art"
+            >
+                <rect id="canvas" width="100%" height="100%" fill="white" />
+            </svg>
+            <section className="panel">
+                <section id="shape">
+                    <label>
+                        Circle
+                        <input type="radio" name="shape" value="circle" checked />
+                    </label>
+                    <label>
+                        Square <input type="radio" name="shape" value="square" />
+                    </label>
+                    <label>
+                        Erase <input type="radio" name="shape" value="erase" />
+                    </label>
+                </section>
+                <section id="size">
+                    <h3>Size</h3>
+                    <input type="range" min="1" max="300" value="50" />
+                </section>
+                <section id="color">
+                    <h3>Color</h3>
+                    <input type="color" value="#cccccc" />
+                </section>
+                <section id="mint">
+                    <button>
+                        Open Wallet
+                    </button>
+                    <button disabled>
+                        Create
+                    </button>
+                </section>
+            </section>
+        </div>
+    );
 };
 
 export default App;
