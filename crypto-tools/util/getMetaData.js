e37ce8b2a56a05468e52ebaf2e8185e1c5e8eaa6
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2025-01-02 20:53:08 +0000

    add test lib.s and test ControlPanel

--- crypto-tools/util/getMetaData.js
+++
@@ -1,7 +0,0 @@
-const getMetaData = (IpfsHash) => ({
-  name: "Aaarto",
-  description: "Aaarto is Art",
-  image: `https://ipfs.io/ipfs/${IpfsHash}/aaarto.svg`,
-});
-
-export default getMetaData;
---
+++ setupTests.js
@@ -0,0 +1,4 @@
+// setupTests.js
+// require('@testing-library/jest-dom/extend-expect');
+// setupTests.js
+require('@testing-library/jest-dom');
