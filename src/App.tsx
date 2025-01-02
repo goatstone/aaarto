096610c3ad23946cf191814c6db335e6434e6bd7
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2025-01-02 19:47:15 +0000

    Refactor out ControlPanel from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,27 @@
 import React, { useState } from 'react';
 import Canvas from './Canvas';
 import Header from './Header';
+import ControlPanel from './ControlPanel';
 import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnect';
 
 const App: React.FC = () => {
 
-    const [size, setSize] = useState<number>(70);
     const [shape, setShape] = useState<string>('circle');
+    const [size, setSize] = useState<number>(70);
     const [color, setColor] = useState<string>('#cccccc');
 
     return (
         <div>
             <Header />
-            <Canvas size={size} shape={shape} color={color} />
-            <section className="panel">
-                <section id="shape">
-                    <label>
-                        Circle
-                        <input type="radio" name="shape" value="circle"
-                            checked={shape === 'circle'}
-                            onChange={({ target }) => setShape(target.value)}
-                        />
-                    </label>
-                    <label>
-                        Square <input type="radio" name="shape" value="square"
-                            checked={shape === 'square'}
-                            onChange={({ target }) => setShape(target.value)}
-                        />
-                    </label>
-                    <label>
-                        Erase <input type="radio" name="shape" value="erase"
-                            checked={shape === 'erase'}
-                            onChange={({ target }) => setShape(target.value)}
-                        />
-                    </label>
-                </section>
-                <section id="size">
-                    <h3>Size</h3>
-                    <input type="range" value={size} onChange={({ target }) => setSize(Number(target.value))} min="10" max="300" />
-                </section>
-                <section id="color">
-                    <h3>Color</h3>
-                    <input onChange={({ target }) => setColor(target.value)} type="color" value={color} />
-                </section>
-                <section id="mint">
-                    <button>
-                        Open Wallet
-                    </button>
-                    <button disabled>
-                        Create
-                    </button>
-                </section>
-            </section>
+            <Canvas shape={shape} size={size} color={color} />
+            <ControlPanel
+                shape={shape}
+                setShape={setShape}
+                size={size}
+                setSize={setSize}
+                color={color}
+                setColor={setColor}
+            />
         </div>
     );
 };
---
+++ src/ControlPanel.tsx
@@ -0,0 +1,80 @@
+import React from "react"
+
+type ControlPanelProps = {
+    shape: string,
+    setShape: React.Dispatch<React.SetStateAction<string>>,
+    size: number,
+    setSize: React.Dispatch<React.SetStateAction<number>>,
+    color: string,
+    setColor: React.Dispatch<React.SetStateAction<string>>
+};
+
+const ControlPanel: React.FC<ControlPanelProps> = ({ shape, setShape, size, setSize, color, setColor }) => {
+
+    return (
+        <section className="panel">
+            <section id="shape">
+                <label>
+                    Circle
+                    <input
+                        type="radio"
+                        name="shape"
+                        value="circle"
+                        checked={shape === 'circle'}
+                        onChange={({ target }) => setShape(target.value)}
+                    />
+                </label>
+                <label>
+                    Square
+                    <input
+                        type="radio"
+                        name="shape"
+                        value="square"
+                        checked={shape === 'square'}
+                        onChange={({ target }) => setShape(target.value)}
+                    />
+                </label>
+                <label>
+                    Erase
+                    <input
+                        type="radio"
+                        name="shape"
+                        value="erase"
+                        checked={shape === 'erase'}
+                        onChange={({ target }) => setShape(target.value)}
+                    />
+                </label>
+            </section>
+            <section id="size">
+                <label>
+                    Size
+                    <input
+                        type="range"
+                        value={size}
+                        min="10"
+                        max="300"
+                        onChange={({ target }) => setSize(Number(target.value))}
+                    />
+                </label>
+            </section>
+            <section id="color">
+                <label>Color
+                    <input
+                        type="color"
+                        value={color}
+                        onChange={({ target }) => setColor(target.value)}
+                    />
+                </label>
+            </section>
+            <section id="mint">
+                <button>
+                    Open Wallet
+                </button>
+                <button disabled>
+                    Create
+                </button>
+            </section>
+        </section>
+    )
+}
+export default ControlPanel;
