5ea8d15b147e3c7fbb7913c075e873a2d020a4fa
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2025-01-02 16:22:54 +0000

    Update App with interface and removeElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import React, { useState } from 'react';
+import React, { JSX, ReactElement, useEffect, useState } from 'react';
 import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnect';
 
+interface SVGElementProps {
+    shape: 'circle' | 'rect' | 'erase';
+    size: number;
+}
+
 const App: React.FC = () => {
+
     const [size, setSize]: any = useState(70);
-    const [shape, setShape]: any = useState('circle');
-    const [SVGElements, setSVGElements] = useState<any[]>([]);
-    const handleCanvasClick = (e) => {
-        const rect = e.target.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const el = <circle cx={x} cy={y} r={size / 2} fill="red" />
-        setSVGElements(previousElements => ([...previousElements, el]))
+    const [shape, setShape] = useState<string>('circle');
+    const [SVGElements, setSVGElements] = useState<JSX.Element[]>([]);
+
+    const removeElement = (id: string) => {
+        setSVGElements(prev => prev.filter((el: any) => {
+            return el.props.id !== id
+        }))
+
+    }
+    const handleCanvasClick = (event: any) => {
+        const generatedID = Date.now().toString();
+        let newElement: any;
+        const target = event.currentTarget as any;
+        const rect = target.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        if (shape === 'circle') {
+            newElement = <circle cx={x} cy={y} r={size / 2} fill="red" id={generatedID} />
+            setSVGElements(previousElements => ([...previousElements, newElement]))
+        }
+        else if (shape === 'square') {
+            newElement = <rect x={x - size / 2} y={y - size / 2} width={size} height={size} id={generatedID} />
+            setSVGElements(previousElements => ([...previousElements, newElement]))
+        }
+        else {
+            removeElement(event.target.id)
+        }
     }
     const handleShapeChange = ({ target }: any) => {
         setShape(target.value);
@@ -19,11 +44,10 @@ const App: React.FC = () => {
         return (<svg className='canvas'
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
-            id="art"
             onClick={handleCanvasClick}
         >
             <rect id="canvas" width="100%" height="100%" fill="white" />
-            {SVGElements.map((item: any) => (item))}
+            {SVGElements.map(item => item)}
         </svg>)
     };
 
@@ -31,7 +55,7 @@ const App: React.FC = () => {
         <div>
             <h1>
                 <a href="https://github.com/JoseHerminioCollas/aaarto/" target="new">
-                    Aaarto
+                    Aaarto {shape}
                 </a>
                 <a href="http://goatstone.com" target="new">goatstone</a>
             </h1>
