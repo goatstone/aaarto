fca5ee4dec2b7bb7df33023e748097d33add8837
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2025-01-02 19:13:25 +0000

    Refactor the Canvas element out of the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
-import React, { JSX, ReactElement, useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Canvas from './Canvas';
 import { HashConnect, HashConnectConnectionState, SessionData } from 'hashconnect';
 
 const App: React.FC = () => {
 
     const [size, setSize] = useState<number>(70);
     const [shape, setShape] = useState<string>('circle');
-    const [color, setColor] = useState<string>('#cccccc')
-    const [SVGElements, setSVGElements] = useState<JSX.Element[]>([]);
-    const removeElement = (id: string) => {
-        setSVGElements(prev => prev.filter((el: JSX.Element) => { return el.props.id !== id }));
-    }
-    const handleCanvasClick = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
-        const generatedID = Date.now().toString();
-        let newElement: JSX.Element | null = null;
-        const rect = (event.currentTarget as SVGSVGElement).getBoundingClientRect();
-        const x: number = event.clientX - rect.left;
-        const y: number = event.clientY - rect.top;
-        if (shape === 'erase') {
-            const target = event.target as SVGElement;
-            if (target.id) {
-                removeElement(target.id);
-            }
-            return;
-        }
-        else if (shape === 'circle') {
-            newElement = <circle cx={x} cy={y} r={size / 2} fill={color} id={generatedID} />;
-        }
-        else if (shape === 'square') {
-            newElement = <rect x={x - size / 2} y={y - size / 2} fill={color} width={size} height={size} id={generatedID} />;
-        }
-        if (newElement !== null) {
-            setSVGElements(previousElements => ([...previousElements, newElement]));
-        }
-    }
-    const Canvas: React.FC = () => {
-        return (<svg className='canvas'
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={handleCanvasClick}
-        >
-            <rect id="canvas" width="100%" height="100%" fill="white" />
-            {SVGElements.map(item => item)}
-        </svg>);
-    };
+    const [color, setColor] = useState<string>('#cccccc');
 
     return (
         <div>
@@ -52,7 +16,7 @@ const App: React.FC = () => {
                 </a>
                 <a href="http://goatstone.com" target="new">goatstone</a>
             </h1>
-            <Canvas />
+            <Canvas size={size} shape={shape} color={color} />
             <section className="panel">
                 <section id="shape">
                     <label>
