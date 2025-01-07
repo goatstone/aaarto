72d15b132b450a0ed6f575d83bcae0a01b679d0f
Jose Herminio Collas <JoseHerminioCollas@goatstone.com>
2025-01-07 18:32:43 +0000

    Use MetaMask hook in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Canvas from '@components/Canvas';
 import Header from '@components/Header';
 import ControlPanel from '@components/ControlPanel';
+import useMetaMask from 'hooks/useMetaMask';
 
 const App: React.FC = () => {
+  const [shape, setShape] = useState<string>('circle');
+  const [size, setSize] = useState<number>(70);
+  const [color, setColor] = useState<string>('#cccccc');
+  const { account, connectWallet, openMetaMask } = useMetaMask();
 
-    const [shape, setShape] = useState<string>('circle');
-    const [size, setSize] = useState<number>(70);
-    const [color, setColor] = useState<string>('#cccccc');
+  useEffect(() => { connectWallet() }, []);
 
-    return (
-        <div>
-            <Header />
-            <Canvas shape={shape} size={size} color={color} />
-            <ControlPanel
-                shape={shape}
-                setShape={setShape}
-                size={size}
-                setSize={setSize}
-                color={color}
-                setColor={setColor}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Canvas shape={shape} size={size} color={color} />
+      <p>Connected Account: {account}</p>
+      <button onClick={connectWallet}>
+        Connect to MetaMask
+      </button>
+      <button onClick={openMetaMask}>
+        Open MetaMask
+      </button>
+      <ControlPanel
+        shape={shape}
+        setShape={setShape}
+        size={size}
+        setSize={setSize}
+        color={color}
+        setColor={setColor}
+      />
+    </div>
+  );
 };
 
 export default App;
