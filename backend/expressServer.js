ae2ddddcb7be31261020e2df5414dd29253251a9
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-01-26 11:25:45 -0500

    Rename folder, add OR to process variables

--- backend/expressServer.js
+++ backend/expressServer.js
@@ -2,14 +2,14 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const FormData = require('form-data');
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 const port = 5000;
-// const PINATA_API_KEY = process.env.PINATA_API_KEY;
-// const PINATA_SECRET_API_KEY = process.env.PINATA_SECRET_API_KEY;
+const PINATA_API_KEY = process.env.PINATA_API_KEY || '';
+const PINATA_SECRET_API_KEY = process.env.PINATA_SECRET_API_KEY || '';
 
 app.get('/', (req, res) => {
   res.send('xxx')
@@ -37,8 +37,9 @@ app.post('/upload', async (req, res) => {
     // Update metadata image field with the retrieved IPFS hash
     // metadata.image = `ipfs://${ipfsHash}`;
 
+    // TODO : restore following line when server is complete
     // return res.status(200).json({ ipfsHash, metadata });
-    return res.status(200).json({ metadata })
+    return res.status(200).json({ ipfsHash: "xxx", metadata })
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Failed to upload to Pinata' });
