dd093c201a1250130174447c25aca236c575263e
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-01 14:37:26 -0500

    Update test for App

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,69 +1,22 @@
-// Jest encountered an unexpected token
-
-// Jest failed to parse a file. This happens e.g. when your code or its dependencies use non-standard JavaScript syntax, or when Jest is not configured to support such syntax.
-
-// Out of the box Jest supports Babel, which will be used to transform your files into valid JS based on your Babel configuration.
-
-// By default "node_modules" folder is ignored by transformers.
-
-// Here's what you can do:
-//  • If you are trying to use ECMAScript Modules, see https://jestjs.io/docs/ecmascript-modules for how to enable it.
-//  • If you are trying to use TypeScript, see https://jestjs.io/docs/getting-started#using-typescript
-//  • To have some of your "node_modules" files transformed, you can specify a custom "transformIgnorePatterns" in your config.
-//  • If you need a custom transformation specify a "transform" option in your config.
-//  • If you simply want to mock your non-JS modules (e.g. binary assets) you can stub them out with the "moduleNameMapper" config option.
-
-// You'll find more details and examples of these config options in the docs:
-// https://jestjs.io/docs/configuration
-// For information about custom transformations, see:
-// https://jestjs.io/docs/code-transformation
-
-// Details:
-
-// /workspaces/aaarto/node_modules/uuid/dist/esm-browser/index.js:1
-// ({"Object.<anonymous>":function(module,exports,require,__dirname,__filename,jest){export { default as v1 } from './v1.js';
-//                                                                                   ^^^^^^
-
-// SyntaxError: Unexpected token 'export'
-
-//   1 | import { useEffect, useState } from 'react';
-// > 2 | import { MetaMaskSDK } from "@metamask/sdk";
-//     | ^
-//   3 |
-//   4 | const useMetaMask = () => {
-//   5 |     const [account, setAccount] = useState<string | null>(null);
-
-//   at Runtime.createScriptFromCode (node_modules/jest-runtime/build/index.js:1505:14)
-//   at Object.<anonymous> (node_modules/@metamask/sdk/dist/node/cjs/metamask-sdk.js:1:293)
-//   at Object.<anonymous> (src/hooks/useMetaMask.ts:2:1)
-//   at Object.<anonymous> (src/components/App.tsx:5:1)
-//   at Object.<anonymous> (tests/App.test.tsx:4:1)
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-// import App from '@components/App';
-const App = ()=>{return ""}
+import App from '@components/App';
 
-// Test suite for the App component
-// SKIP this test until a solution is found for the above issue :  Jest encountered an unexpected token
-describe.skip('App', () => {
-  // Test to check if the Header component is rendered
-  test('renders Header component', () => {
+describe('App', () => {
+
+  test('should render expected text', () => {
     render(<App />);
-    expect(screen.getByText(/aaarto/i)).toBeInTheDocument();
+    expect(screen.queryAllByText(/aaarto/i).length).toBe(2);
   });
 
-  // Test to check if the Canvas component is rendered with initial props
-  test('renders Canvas component with initial props', () => {
-    render(<App />);
-    const canvasElement = screen.getByTestId('canvas');
-    expect(canvasElement).toHaveAttribute('data-shape', 'circle');
-    expect(canvasElement).toHaveAttribute('data-size', '70');
-    expect(canvasElement).toHaveAttribute('data-color', '#cccccc');
+  test('should render an SVG tag', () => {
+    const { container } = render(<App />);
+    const a = container.getElementsByTagName('svg')[0];
+    expect(a).toBeInTheDocument()
   });
 
-  // Test to check if the ControlPanel component is rendered with initial props
-  test('renders ControlPanel component with initial props', () => {
+  test('should render ControlPanel component with initial props', () => {
     render(<App />);
     expect(screen.getByLabelText('Circle')).toBeChecked();
     expect(screen.getByLabelText('Size')).toHaveValue('70');
