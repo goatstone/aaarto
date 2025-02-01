c764ecb14593ff7105cb0591c8a6ef875eae73a9
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-01 10:51:45 -0500

    Incorporate components to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Canvas from '@components/Canvas';
 import Header from '@components/Header';
-import ControlPanel from '@components/ControlPanel';
-import useMetaMask from '@hooks/useMetaMask';
+import CanvasControl from '@components/CanvasControl';
+import TitleControl from '@components/TitleControl';
+import MintControl from './MintControl';
+import Message from './Message';
 import useMintNFT from '@hooks/useMintNFT';
 import useUpload from '@hooks/useUpload';
 
@@ -25,7 +27,7 @@ const App: React.FC = () => {
       console.log('SVG or Title is missing');
       return false;
     }
-    const ipfsHashMD = ''
+    const ipfsHashMD = '';
     // const ipfsHashMD = await uploadToServer(svgString, title);
     // if (ipfsHashMD) {
     await mintNFT(`ipfs://${ipfsHashMD}`);
@@ -37,24 +39,35 @@ const App: React.FC = () => {
     <div>
       <Header />
       <Canvas shape={shape} size={size} color={color} setSvgString={setSvgString} />
-      <ControlPanel
-        account={account}
-        handleUpload={handleUpload}
-        uploading={uploading}
-        loading={loading}
-        shape={shape}
-        setShape={setShape}
-        size={size}
-        setSize={setSize}
-        color={color}
-        setColor={setColor}
-        transactionHash={transactionHash}
-        transactionReceipt={transactionReceipt}
-        errorMessage={errorMessage}
-        uploadError={uploadError}
-        title={title}
-        setTitle={setTitle}
-      />
+      <section className='controls'>
+
+        <CanvasControl
+          shape={shape}
+          setShape={setShape}
+          size={size}
+          setSize={setSize}
+          color={color}
+          setColor={setColor}
+        />
+        <TitleControl title={title}
+          setTitle={setTitle}
+        />
+        <MintControl
+          handleUpload={handleUpload}
+          loading={loading}
+          uploading={uploading}
+        />
+        <Message
+          account={account}
+          uploading={uploading}
+          loading={loading}
+          transactionHash={transactionHash}
+          transactionReceipt={transactionReceipt}
+          errorMessage={errorMessage}
+          uploadError={uploadError}
+        />
+      </section>
+
     </div>
   );
 };
