d688aefe58a1e2418b843bd7c1f5c4972d64868e
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-24 06:48:03 -0500

    Update types to recognize the windows env prop

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,26 @@
 import contractArtifactSepolia from "../artifacts/contracts/AaartoNFTV4.sol/AaartoNFTV4.json";
 
 /**
- * config: if there is a window.env g
+ * config: if there is a window.env
  * get values from it, otherwise use default values
  */
+export type WindowEnv = { platformFee: string; network: string };
+
 let config: any;
-// if there is a window env file get the network field
-// otherwise use the default network
 // TODO test only, remove the next line for production
-//@ts-ignore
-window.env = { network: "sepolia", platformFee: "0.002" };
+const windowEnv: WindowEnv = { network: "sepolia", platformFee: "0.002" };
+window.env = windowEnv;
+// set default values
 let platformFee = "0.001";
 let network = "sepolia";
-// @ts-ignore
 if (window.env) {
-  // @ts-ignore
   platformFee = window.env.platformFee;
-  // @ts-ignore
   network = window.env.network;
 }
 if (network === "sepolia") {
-  //@ts-ignore
   console.log(window.env.network);
   config = {
-    contractArtifact:contractArtifactSepolia,
+    contractArtifact: contractArtifactSepolia,
     platformFee,
     contractAddress: "0x92128cD1BCA8cc406d2223Dcf1558E4d926Dd68f",
     chainIDHex: "0xaa36a7",
